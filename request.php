cc47937b4d48d0c95bf8051982937c1a7057dd47
AprilHall <april21abs@gmail.com>
2017-10-30 22:25:18 -0500

    second commit

--- request.php
+++ request.php
@@ -4,12 +4,12 @@
 $query = $_GET['q'];
 $definition = [
     "definition" => "A statement of the exact meaning of a word, especially in a dictionary.",
-    "bar" => "A place that sells alcholic beverages",
-    "ajax" => "Technique which involves the use of javascript and xml (or JSON)",
+    "bar" => "A place that sells alcholic beverages.",
+    "ajax" => "Technique which involves the use of javascript and xml (or JSON).",
     "html" => "The standard markup language for creating web pages and web applications.",
     "css" => "A style sheet language used for describing the presentation of a document written in a markup language.",
     "javascript" => "A lightweight, interpreted programming language with first-class functions that adds interactivity to your website.",
-    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites",
+    "php" => "A server-side scripting language, and a powerful tool for making dynamic and interactive websites.",
 ];
 print "<h3>" . strtoupper($query) . "</h3>";
 print "<p>" . $definition[$query] . "</p>";
